0b56a110c6ebe1ea2b383d3f654914bee6d5bc8e
jorgealexis07 <201711104@tese.edu.mx>
2022-05-12 14:09:23 -0500

    Corregi codigo para Habilitar un endpoint para consultar todos los estudiantes con todos sus campos.

--- test/utils/Reader.test.js
+++ test/utils/Reader.test.js
@@ -3,13 +3,13 @@ const Reader = require("./../../lib/utils/Reader");
 
 describe("Tests para Reader", () => {
     test("Requerimiento 1: probar cuando se envia una ruta invalida", () => {
-        const explorers = Reader.readJsonFile("visualpartner.json");
-        expect(explorers).toBe("visualpartner.json");
+        const students = Reader.readJsonFile("visualpartner.json");
+        expect(students).toBe("visualpartner.json");
     });
 
     test("Requerimiento 2: probar cuando se envia una ruta valida", () => {
-        const explorers = Reader.readJsonFile("visualpartners.json");
-        expect(explorers);
+        const students = Reader.readJsonFile("visualpartners.json");
+        expect(students);
     });
 
 });
